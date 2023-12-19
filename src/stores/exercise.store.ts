eaa4f36761ef35d3fceecb324c8448c7d6dd52e2
NamVN <nhatnam.0127@gmail.com>
2023-12-19 23:37:27 +0700

    Feature: rating

--- src/stores/exercise.store.ts
+++ src/stores/exercise.store.ts
@@ -1,11 +1,14 @@
 import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 import { getExerciseById } from '@/api/exercise.api';
+import type { Rating } from '@/types/interfaces/rating';
+import { ratingApi } from '@/api/rating.api';
 
 export const useExerciseStore = defineStore('exerciseStore', () => {
   const exercise = ref<any>('');
   const exerciseHintCode = ref<any>('');
   const exerciseFlag = ref<boolean>(false); // use to check if exercise data is ready
+  const averageRating = ref<number>(0);
 
   const getExerciseDetail = async (exerciseId: string) => {
     await getExerciseById(exerciseId)
@@ -13,16 +16,25 @@ export const useExerciseStore = defineStore('exerciseStore', () => {
         exercise.value = res?.data;
         exerciseHintCode.value = res?.data.hintCode;
         exerciseFlag.value = true;
+        averageRating.value = exercise.value.rating;
       })
       .catch((error: any) => {
 
       })
   };
 
+  const createRating = async (rating: Rating) => {
+    try {
+      await ratingApi(rating);
+    } catch(error : any) {}
+  }
+
   return {
     exerciseHintCode,
     exercise,
     exerciseFlag,
-    getExerciseDetail
+    averageRating,
+    getExerciseDetail,
+    createRating
   }
 })
